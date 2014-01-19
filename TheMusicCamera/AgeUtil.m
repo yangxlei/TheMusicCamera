0117e7b072f6305861ab4141b89fe06e2ce35c23
yanglei <yanglei@yangleimatoMacBook-Pro.local>
2014-01-19 21:57:42 +0800

    age stamp

--- TheMusicCamera/AgeUtil.m
+++ TheMusicCamera/AgeUtil.m
@@ -13,7 +13,7 @@
 +(UIImage*) generateAgeStampImage:(int)item andYear:(int)year andMonth:(int)month
 {
   UIImage* stamp = [UIImage imageNamed:[NSString stringWithFormat:@"age_stamp_%d.png", item]];
-  UIImage* number = [UIImage imageNamed:[NSString stringWithFormat:@"age_stamp_%d_number.png", item]];
+//  UIImage* number = [UIImage imageNamed:[NSString stringWithFormat:@"age_stamp_%d_number.png", item]];
   
   if (year < 0 || month < 0)
   {
@@ -23,35 +23,35 @@
   UIImage* yearImg, *monthImg;
   CGRect rect1, rect2;
   
+  yearImg = [self getNumberWith:year andItem:item];
+  monthImg = [self getNumberWith:month andItem:item];
 
   
   switch (item) {
     case 1:
-      yearImg = [self getNumber:year inImage:number withOffset:0];
-      monthImg = [self getNumber:month inImage:number withOffset:0];
-     rect1.origin.x = 110;
+     rect1.origin.x = 153 -yearImg.size.width;
       rect1.origin.y = 148;
       rect1.size = yearImg.size;
       
-      rect2.origin.x = 110;
+      rect2.origin.x = 150-monthImg.size.width;
       rect2.origin.y = 212;
       rect2.size = monthImg.size;
       break;
     case 2:
-      yearImg = [self getNumber:year inImage:number withOffset:0];
-      monthImg = [self getNumber:month inImage:number withOffset:0];
+//      yearImg = [self getNumber:year inImage:number withOffset:0];
+//      monthImg = [self getNumber:month inImage:number withOffset:0];
       
-      rect1.origin.x = 26;
+      rect1.origin.x = 57 - yearImg.size.width;
       rect1.origin.y = 13;
       rect1.size = yearImg.size;
       
-      rect2.origin.x = 202;
+      rect2.origin.x = 232 - monthImg.size.width;
       rect2.origin.y = 13;
       rect2.size = monthImg.size;
       break;
     case 3:
-      yearImg = [self getNumber:year inImage:number withOffset:0];
-      monthImg = [self getNumber:month inImage:number withOffset:0];
+//      yearImg = [self getNumber:year inImage:number withOffset:0];
+//      monthImg = [self getNumber:month inImage:number withOffset:0];
       
       rect1.origin.x = 26;
       rect1.origin.y = 27;
@@ -62,62 +62,62 @@
       rect2.size = monthImg.size;
       break;
     case 4:
-      yearImg = [self getNumber:year inImage:number withOffset:15];
-      monthImg = [self getNumber:month inImage:number withOffset:15];
+//      yearImg = [self getNumber:year inImage:number withOffset:15];
+//      monthImg = [self getNumber:month inImage:number withOffset:15];
       
-      rect1.origin.x = 95;
+      rect1.origin.x = 154 - yearImg.size.width;
       rect1.origin.y = 64;
       rect1.size = yearImg.size;
       
-      rect2.origin.x = 105;
+      rect2.origin.x = 157 - monthImg.size.width;
       rect2.origin.y = 140;
       rect2.size = monthImg.size;
       break;
     case 5:
-      yearImg = [self getNumber:year inImage:number withOffset:0];
-      monthImg = [self getNumber:month inImage:number withOffset:0];
+//      yearImg = [self getNumber:year inImage:number withOffset:0];
+//      monthImg = [self getNumber:month inImage:number withOffset:0];
       
-      rect1.origin.x = 53;
+      rect1.origin.x = 74 - yearImg.size.width;
       rect1.origin.y = 72;
       rect1.size = yearImg.size;
       
-      rect2.origin.x = 48;
+      rect2.origin.x = 70 - monthImg.size.width;
       rect2.origin.y = 110;
       rect2.size = monthImg.size;
       break;
     case 6:
-      yearImg = [self getNumber:year inImage:number withOffset:12];
-      monthImg = [self getNumber:month inImage:number withOffset:12];
+//      yearImg = [self getNumber:year inImage:number withOffset:12];
+//      monthImg = [self getNumber:month inImage:number withOffset:12];
       
-      rect1.origin.x = 202;
+      rect1.origin.x = 232 - yearImg.size.width;
       rect1.origin.y = 53;
       rect1.size = yearImg.size;
       
-      rect2.origin.x = 205;
+      rect2.origin.x = 235 - monthImg.size.width;
       rect2.origin.y = 85;
       rect2.size = monthImg.size;
       break;
     case 7:
-      yearImg = [self getNumber:year inImage:number withOffset:3];
-      monthImg = [self getNumber:month inImage:number withOffset:3];
+//      yearImg = [self getNumber:year inImage:number withOffset:3];
+//      monthImg = [self getNumber:month inImage:number withOffset:3];
       
-      rect1.origin.x = 129;
+      rect1.origin.x = 168 - yearImg.size.width;
       rect1.origin.y = 37;
       rect1.size = yearImg.size;
       
-      rect2.origin.x = 129;
+      rect2.origin.x = 172 - monthImg.size.width;
       rect2.origin.y = 85;
       rect2.size = monthImg.size;
       break;
     case 8:
-      yearImg = [self getNumber:year inImage:number withOffset:5];
-      monthImg = [self getNumber:month inImage:number withOffset:5];
+//      yearImg = [self getNumber:year inImage:number withOffset:5];
+//      monthImg = [self getNumber:month inImage:number withOffset:5];
       
-      rect1.origin.x = 150;
+      rect1.origin.x = 171 - yearImg.size.width;
       rect1.origin.y = 17;
       rect1.size = yearImg.size;
       
-      rect2.origin.x = 152;
+      rect2.origin.x = 179 - monthImg.size.width;
       rect2.origin.y = 72;
       rect2.size = monthImg.size;
       break;
@@ -152,7 +152,7 @@
 {
   CGRect rect;
   
-  CGFloat width = image.size.width /10 ;
+  CGFloat width = image.size.width /11;
   
 	rect.origin.x = number*width - offset;
 	rect.origin.y = 0;
@@ -160,6 +160,33 @@
 	rect.size.height = image.size.height;
 	
 	CGImageRef cr = CGImageCreateWithImageInRect(image.CGImage, rect);
+	
+	UIImage *cropped = [UIImage imageWithCGImage:cr];
+  
+	
+	CGImageRelease(cr);
+  return cropped;
+}
+
++(UIImage*) getNumberWith:(int) number andItem:(int) item {
+  CGRect rect ;
+  NSDictionary* plist = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"ages" ofType:@"plist"]];
+  
+  NSArray* age = [plist valueForKey:[NSString stringWithFormat:@"age_%d",item]];
+  
+  NSDictionary* info = [age objectAtIndex:number];
+  
+  int start = [[info valueForKey:@"start"] intValue];
+  int end = [[info valueForKey:@"end"] intValue];
+  
+  UIImage* img = [UIImage imageNamed:[NSString stringWithFormat:@"age_stamp_%d_number.png", item]];
+  
+  rect.origin.x = start;
+  rect.origin.y = 0 ;
+  rect.size.width = end - start;
+  rect.size.height = img.size.height;
+  
+ 	CGImageRef cr = CGImageCreateWithImageInRect(img.CGImage, rect);
 	
 	UIImage *cropped = [UIImage imageWithCGImage:cr];
   
