62db46be7a62eb3ab815cd4449cb38f599f2437a
franksong1988 <317295583@qq.com>
2014-02-24 21:20:06 +0800

    ，，，，，

--- TheMusicCamera/camera/CameraController.m
+++ TheMusicCamera/camera/CameraController.m
@@ -377,12 +377,15 @@
              [self performSelector:@selector(resetCamear) withObject:nil afterDelay:0.8];
          }];
         
-        
-        UIGraphicsBeginImageContext(self.cameraView.bounds.size);     //currentView 当前的view  创建一个基于位图的图形上下文并指定大小为
-        [self.cameraView.layer renderInContext:UIGraphicsGetCurrentContext()];//renderInContext呈现接受者及其子范围到指定的上下文
-        UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();//返回一个基于当前图形上下文的图片
-        UIGraphicsEndImageContext();//移除栈顶的基于当前位图的图形上下文
-        UIImageWriteToSavedPhotosAlbum(viewImage, nil, nil, nil);//然后将该图片保存到图片图
+//        UIGraphicsBeginImageContext(self.cameraView.bounds.size);     //currentView 当前的view  创建一个基于位图的图形上下文并指定大小为
+//        [self.cameraView.layer renderInContext:UIGraphicsGetCurrentContext()];//renderInContext呈现接受者及其子范围到指定的上下文
+//        UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();//返回一个基于当前图形上下文的图片
+//        UIGraphicsEndImageContext();//移除栈顶的基于当前位图的图形上下文
+//        UIImageWriteToSavedPhotosAlbum(viewImage, nil, nil, nil);//然后将该图片保存到图片图
+
+    }
+    else
+    {
     }
 
 }
